3bc4108450d6ffe46977ea0c650db68364041323
Damian Yerrick <git@pineight.com>
2018-06-02 20:47:20 -0400

    compress mono tilemaps as incruniq (.iu)
    It's not quite a miracle, but turning runs of map entries
    representing singleton tiles into runs before pb16 on the tilemap
    saved about 100 bytes.  I should be able to eke out more by
    compressing the GBC version of the Gus portrait, after which I'll
    have 1K which is enough to start writing the menu for the GBC tests.

--- gameboy/tools/incruniq.py
+++ gameboy/tools/incruniq.py
@@ -18,27 +18,71 @@ size  tile  sngl  reus  name
 If we can beat PB16, which compresses Green Hill Zone's tilemap
 from 576 bytes to 398, it's already worthwhile.
 
+The file format:
+1 byte: number of PB8 compressed tiles (or 0 for 256)
+PB16 compressed tiles
+1 byte: length of singleton-eliminated map, divided by 16 and rounded up
+1 byte: starting tile number of singletons
+PB16 compressed singleton-eliminated map
+
 """
 from collections import Counter
+import sys
+import argparse
+from pb16 import pb16
 
-def frequniq(it):
+def incruniq(it):
     seq = list(it)
+
+    # Find members used only once
     freqs = Counter(seq)
     byfreq = freqs.most_common()
     singletons = set(el for el, freq in byfreq if freq < 2)
-    print("%d entries, %d unique, %d singletons, %d reused"
-          % (len(seq), len(freqs), len(singletons),
-             len(freqs) - len(singletons)))
+    freqs = byfreq = None
 
-nams = [
-    "../obj/gb/Gus_portrait.nam",
-    "../obj/gb/linearity.nam",
-    "../obj/gb/sharpness.nam",
-    "../obj/gb/greenhillzone.nam",
-    "../obj/gb/stopwatchface.nam",
-]
-for filename in nams:
-    with open(filename, "rb") as infp:
-        nam = infp.read()
-    print(filename)
-    frequniq(nam)
+    # Find members used more than once in order of first appearance
+    alltiles = []
+    itiles = {el: 0xFF  for el in singletons}
+    for el in seq:
+        if el in itiles: continue
+        itiles[el] = len(alltiles)
+        alltiles.append(el)
+    imultis = None
+
+    # Put singletons in order of sole appearance
+    firstsingleton = len(alltiles)
+    alltiles.extend(el for el in seq if el in singletons)
+    
+    # Calculate the tilemap
+    seq = bytes(itiles[el] for el in seq)
+    return alltiles, firstsingleton, seq
+
+def parse_argv(argv):
+    p = argparse.ArgumentParser()
+    p.add_argument("srcfile")
+    p.add_argument("iufile")
+    return p.parse_args(argv[1:])
+
+def main(argv=None):
+    args = parse_argv(argv or sys.argv)
+    with open(args.srcfile, "rb") as infp:
+        data = infp.read()
+        print(len(data))
+    block_size = 16
+    data = [data[i:i + block_size] for i in range(0, len(data), block_size)]
+    alltiles, firstsingleton, nam = incruniq(data)
+
+    out = bytearray()
+    out.append(len(alltiles))
+    out.extend(b''.join(pb16(b''.join(alltiles))))
+    nampb16size = -(-len(nam) // 16)
+    out.append(nampb16size)
+    out.append(firstsingleton)
+    out.extend(b''.join(pb16(nam)))
+    print(len(out))
+    with open(args.iufile, "wb") as outfp:
+        outfp.write(out)
+
+if __name__=='__main__':
+    main()
+##    main(["incruniq.py", "../obj/gb/Gus_portrait.chrgb", "test.iu"])
