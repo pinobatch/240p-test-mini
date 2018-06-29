ce1f4b5e74ecb86ea7a6ed2cb673d3d744c2312b
Damian Yerrick <git@pineight.com>
2018-06-29 17:58:02 -0400

    GBA: add license headers

--- gba/src/pads.c
+++ gba/src/pads.c
@@ -1,3 +1,27 @@
+/*
+Controller reading for Game Boy Advance
+Copyright 2018 Damian Yerrick
+
+This work is provided 'as-is', without any express or implied
+warranty.  In no event will the authors be held liable for any
+damages arising from the use of this work.
+
+Permission is granted to anyone to use this work for any purpose,
+including commercial applications, and to alter it and redistribute
+it freely, subject to the following restrictions:
+
+1. The origin of this work must not be misrepresented; you must
+   not claim that you wrote the original work. If you use
+   this work in a product, an acknowledgment in the product
+   documentation would be appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must
+   not be misrepresented as being the original work.
+3. This notice may not be removed or altered from any source
+   distribution.
+
+"Source" is the preferred form of a work for making changes to it.
+
+*/
 #include <gba_input.h>
 #include "global.h"
 
--- gba/src/soundtest.c
+++ gba/src/soundtest.c
@@ -1,3 +1,22 @@
+/*
+Sound test for 160p Test Suite
+Copyright 2018 Damian Yerrick
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
 #include "global.h"
 #include <gba_input.h>
 #include <gba_sound.h>
@@ -116,10 +135,6 @@ typedef struct ChordVoice {
   unsigned short delaylinestart, delaylineend;
   unsigned char fac1, fac2;
 } ChordVoice;
-
-typedef struct ChordState {
-  
-} ChordState;
 
 #define PCM_NUM_VOICES 6
 #define PCM_PERIOD 924   // 16777216 tstates/s / 18157 samples/s
