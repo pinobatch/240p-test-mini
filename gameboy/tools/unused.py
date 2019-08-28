7a495aeecdd21b31375e0432cd8f8a45bb2dd2e7
Damian Yerrick <git@pineight.com>
2019-08-27 23:42:04 -0400

    GB: scan binary for optimizable HRAM access
    Add an extension to gameboy/tools/unused.py to load the symbol
    file and print the operand of every LD [aaaa], A and LD A, [aaaa]
    where the opcode is in MMIO ($FF00-$FF7F) or HRAM ($FF80-$FFFF).
    A few byte sequences in compressed help text resemble these
    opcodes (EA xx FF and FA xx FF); exclude them because they are
    not used as instructions.
    
    Free space increases to 280 bytes.

--- gameboy/tools/unused.py
+++ gameboy/tools/unused.py
@@ -4,8 +4,45 @@ Long run detector for binary files
 Copyright 2018 Damian Yerrick
 insert zlib license here
 """
+import string
+
+def disassemble_hram_access(opcode, operand, syms):
+    def addrtosym(s):
+        try:
+            return syms[s]
+        except KeyError:
+            return "$%04x" % s
+    if opcode == 0xEA:
+        return "ld [%s], a" % addrtosym(operand)
+    if opcode == 0xFA:
+        return "ld a, [%s]" % addrtosym(operand)
+
+def load_syms(filename):
+    with open(filename, "r") as infp:
+        lines = [s.split() for s in infp]
+    syms = {}
+    falsepos_ranges = []
+    for line in lines:
+        if len(line) < 2: continue
+        k, v = line[:2]
+        k = k.rsplit(':', 1)[-1]
+        if not k or not v: continue
+        if not all(c in string.hexdigits for c in k): continue
+        k = int(k, 16)
+        syms[k] = v
+
+        # Compressed help text contains byte sequences that
+        # resemble optimizable opcodes
+        if v == 'helppage_000':
+            falsepos_ranges.append((k, k))
+        if v == 'help_cumul_pages':
+            falsepos_ranges[-1] = (falsepos_ranges[-1][0], k)
+    return syms, falsepos_ranges
+
 with open("gb240p.gb", "rb") as infp:
     data = infp.read()
+syms, falsepos_ranges = load_syms("gb240p.sym")
+print(falsepos_ranges)
 
 runthreshold = 32
 runbyte, runlength, runs = 0xC9, 0, []
@@ -24,3 +61,14 @@ for startaddr, endaddr in runs:
     totalsz += sz
     print("%04x-%04x: %d" % (startaddr, endaddr - 1, sz))
 print("total: %d bytes (%.1f KiB)" % (totalsz, totalsz / 1024.0))
+
+optimizable_hram_accesses = [
+    "$%04x: %s"
+    % (i, disassemble_hram_access(d, data[i + 1] + data[i + 2] * 0x100, syms))
+    for i, d in enumerate(data[:-2])
+    if d in (0xEA, 0xFA) and data[i + 2] == 0xFF
+    and not any(l <= i <= h for l, h in falsepos_ranges)
+]
+if optimizable_hram_accesses:
+    print("These HRAM accesses can be optimized:")
+    print("\n".join(optimizable_hram_accesses))
