037436f9f61a98cc74c2bf806134467d4244a279
Damian Yerrick <git@pineight.com>
2019-09-19 22:27:46 -0400

    add Chroma crosstalk test for SGB

---
+++ gameboy/tools/yuvgen.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from math import sin, cos, pi
+
+for i in range(12):
+    theta = i * pi / 6.0
+    y = 127.5
+    u = cos(theta) * 62.5
+    v = sin(theta) * 62.5
+##    print("yuv is", round(y), round(u), round(v))
+    r = round(y + 1.13983*v)
+    g = round(y - .39465*u - .58060*v)
+    b = round(y + 2.03211*u)
+    print("  drgb $%02x%02x%02x" % (r, g, b))
