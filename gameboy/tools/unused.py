b595d1c9e113ac7d7f00b41005b19d0a5b4318f2
Damian Yerrick <git@pineight.com>
2020-02-26 19:08:40 -0500

    GB: jr *+3 to db $FE
    Instead of unconditionally branching over a 1-byte opcode with
    JR, turn the branch into a CP with the other opcode as its
    operand to save 1 byte in each of 3 cases.  Add a test for this
    to tools/unused.py, avoiding two classes of false alarms.
    
    suggested by @ISSOtm in gbdev Discord

--- gameboy/tools/unused.py
+++ gameboy/tools/unused.py
@@ -6,24 +6,27 @@ insert zlib license here
 """
 import string
 
+def addrtosym(s, syms):
+    try:
+        return syms[s]
+    except KeyError:
+        return "$%04x" % s
+
 def disassemble_inst(opcode, operand, syms):
-    def addrtosym(s):
-        try:
-            return syms[s]
-        except KeyError:
-            return "$%04x" % s
     if opcode == 0xEA:
-        return "ld [%s], a" % addrtosym(operand)
+        return "ld [%s], a" % addrtosym(operand, syms)
     if opcode == 0xFA:
-        return "ld a, [%s]" % addrtosym(operand)
+        return "ld a, [%s]" % addrtosym(operand, syms)
     if opcode == 0xCD:
-        return "call %s" % addrtosym(operand)
+        return "call %s" % addrtosym(operand, syms)
+    if opcode == 0x18:
+        return "jr %s" % addrtosym(operand, syms)
 
 falsepos_starts = {
-    'soundtest_handlers', 'helppage_000',
+    'soundtest_handlers', 'helppage_000', 'grayramp_bottomhalfmap',
 }
 falsepos_ends = {
-    'soundtest_8k', 'help_cumul_pages'
+    'soundtest_8k', 'help_cumul_pages', 'grayramp_chr_gbc',
 }
 
 def load_syms(filename):
@@ -79,23 +82,43 @@ optimizable_hram_accesses = [
 
 for i, (addr, inst) in enumerate(optimizable_hram_accesses):
     # LD A, [aaaa] is $FA.  But a backward conditional branch by
-    # 6 bytes is also $FA.  This can cause a false positive with
+    # 6 bytes is also $FA.  This can cause a false alarm with
     # JR NZ, .loop RET (FF-padding) which is 20 FA C9 FF.
     if (addr > 0 and (data[addr - 1] & 0xE7) == 0x20
         and data[addr + 1] == 0xC9):
-        inst = inst + "  ; false positive? (JR before RET)"
+        inst = inst + "  ; false alarm? (JR before RET)"
         optimizable_hram_accesses[i] = (addr, inst)
 
 rstable_calls = [
     (i, disassemble_inst(d, data[i + 1] + data[i + 2] * 0x100, syms))
-    for i, d in enumerate(data[:-2])
+    for i, d in enumerate(data[:-3])
     if d == 0xCD and data[i + 2] == 0 and (data[i + 1] & 0xC7) == 0
     and not any(l <= i <= h for l, h in falsepos_ranges)
 ]
 
+jr_forward_1 = [
+    (i, disassemble_inst(d, data[i + 1] + i + 2, syms))
+    for i, d in enumerate(data[:-2])
+    if d == 0x18 and data[i + 1] == 0x01
+    and not any(l <= i <= h for l, h in falsepos_ranges)
+    and not (
+        addr >= 2 and data[i - 2] == 0xCD
+        and (0x1800 | data[i - 1]) in syms
+    )
+]
+
+for i, (addr, inst) in enumerate(jr_forward_1):
+    # JR *+3 is $18 $01.  But CALL $18xx followed by LD BC, aaaa
+    # is $CD xx $18 $01 xx xx.  This can cause a false alarm.
+    if addr >= 2 and data[addr - 2] == 0xCD:
+        callarg = (data[addr] << 8) | (data[addr - 1])
+        inst = inst + "  ; false alarm? (CALL $%04x then LD BC)" % callarg
+        jr_forward_1[i] = (addr, inst)
+
 optimizable = []
 optimizable.extend(optimizable_hram_accesses)
 optimizable.extend(rstable_calls)
+optimizable.extend(jr_forward_1)
 optimizable.sort()
 
 if optimizable:
