e5b67e17b1a2f3033ff5277dbb316452b500547e
Damian Yerrick <git@pineight.com>
2020-04-08 13:22:10 -0400

    GB unused.py: rename "false positives"
    Wiktionary defines a "false positive" as "a result of a test that
    shows as present something that is absent."  But Twitter users point
    out that readers often confuse a "false positive" with its converse.
    
    Replace "false positive range" with "data range", mirroring the
    previous "false alarm" change I made in b595d1c
    
    References:
    https://twitter.com/ciphernyx/status/1201479840823689216
    https://twitter.com/johnregehr/status/1201558064874184704
    https://twitter.com/ManishEarth/status/1201614731548577792

--- gameboy/tools/unused.py
+++ gameboy/tools/unused.py
@@ -41,11 +41,11 @@ def jr_target(addr, operand):
     return relative_ea(addr + 2, operand)
 
 # Start and end of especially false-alarmy parts of the ROM
-falsepos_starts = {
+datarange_starts = {
     'soundtest_handlers', 'helppage_000', 'grayramp_bottomhalfmap',
     'allhuffdata', 'helptiles', 'waveram_sinx',
 }
-falsepos_ends = {
+datarange_ends = {
     'soundtest_8k', 'help_cumul_pages', 'grayramp_chr_gbc',
     'allhuffdata_end', 'helptiles_end', 'waveram_end',
 }
@@ -54,7 +54,7 @@ def load_syms(filename):
     with open(filename, "r") as infp:
         lines = [s.split() for s in infp]
     syms = {}
-    falsepos_ranges = []
+    dataranges = []
     for line in lines:
         if len(line) < 2: continue
         k, v = line[:2]
@@ -66,15 +66,15 @@ def load_syms(filename):
 
         # Compressed help text and other big data areas may contain
         # byte sequences that resemble optimizable opcodes
-        if v in falsepos_starts:
-            falsepos_ranges.append((k, k))
-        if v in falsepos_ends:
-            falsepos_ranges[-1] = (falsepos_ranges[-1][0], k)
-    return syms, falsepos_ranges
+        if v in datarange_starts:
+            dataranges.append((k, k))
+        if v in datarange_ends:
+            dataranges[-1] = (dataranges[-1][0], k)
+    return syms, dataranges
 
 with open("gb240p.gb", "rb") as infp:
     data = infp.read()
-syms, falsepos_ranges = load_syms("gb240p.sym")
+syms, dataranges = load_syms("gb240p.sym")
 
 runthreshold = 32
 runbyte, runlength, runs = 0xC9, 0, []
@@ -100,7 +100,7 @@ optimizable_hram_accesses = [
     (i, disassemble_inst(d, data[i + 1] + data[i + 2] * 0x100, syms))
     for i, d in enumerate(data[:-2])
     if d in (0xEA, 0xFA) and data[i + 2] == 0xFF
-    and not any(l <= i <= h for l, h in falsepos_ranges)
+    and not any(l <= i <= h for l, h in dataranges)
 ]
 
 for i, (addr, inst) in enumerate(optimizable_hram_accesses):
@@ -116,7 +116,7 @@ rstable_calls = [
     (i, disassemble_inst(d, data[i + 1] + data[i + 2] * 0x100, syms))
     for i, d in enumerate(data[:-3])
     if d == 0xCD and data[i + 2] == 0 and (data[i + 1] & 0xC7) == 0
-    and not any(l <= i <= h for l, h in falsepos_ranges)
+    and not any(l <= i <= h for l, h in dataranges)
 ]
 
 # Optimizable jumps #################################################
@@ -128,18 +128,18 @@ jp_instructions = [
     (i, data[i + 1] + data[i + 2] * 0x100)
     for i, d in enumerate(data[:-3])
     if (d in jp_opcodes
-        and not any(l <= i <= h for l, h in falsepos_ranges))
+        and not any(l <= i <= h for l, h in dataranges))
 ]
 jp_instructions = [
     row for row in jp_instructions
-    if not any(l <= row[1] <= h for l, h in falsepos_ranges)
+    if not any(l <= row[1] <= h for l, h in dataranges)
 ]
 
 jr_instructions = [
     (i, jr_target(i, data[i + 1]))
     for i, d in enumerate(data[:-3])
     if (d in jr_opcodes
-        and not any(l <= i <= h for l, h in falsepos_ranges))
+        and not any(l <= i <= h for l, h in dataranges))
 ]
 
 # Forward 0
@@ -169,7 +169,7 @@ jp_to_ret = [
     if (d in jp_opcodes
         and data[i + 2] < 0x80
         and data[data[i + 1] + data[i + 2] * 0x100] == 0xC9
-        and not any(l <= i <= h for l, h in falsepos_ranges))
+        and not any(l <= i <= h for l, h in dataranges))
 ]
 
 jr_to_ret = [
