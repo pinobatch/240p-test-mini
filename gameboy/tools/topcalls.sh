e6ff9874ad14511e6e34217124d5e7f029e6f7cb
Damian Yerrick <pino@pineight.com>
2021-03-10 07:57:24 -0500

    topcalls.sh: add bottom calls
    TODO: ignore comments in bottom calls

--- gameboy/tools/topcalls.sh
+++ gameboy/tools/topcalls.sh
@@ -7,3 +7,6 @@ grep -h '\scall\s' src/*.z80 | awk '{$1=$1};1' | sort | uniq -c | sort -nr | hea
 
 # then to find all locations of a call, do something like
 # grep -C3 set_bgp src/*.z80
+
+# also things called once (for turning into a macro)
+grep -h '\scall\s' src/*.z80 | awk '{$1=$1};1' | sort | uniq -c | sort -n | head -n10
