9a2d90665ebac2320f2a8839cd787395ab7ed50f
Damian Yerrick <pino@pineight.com>
2023-09-25 09:28:46 -0400

    topcalls.py: filter fallthrough
    - treat savescan.py's fallthrough as an incoming jp for the list
      of routines called once
    - remove callcount.sh which appears to be the previous awk-based
      version of topcalls.py

--- gameboy/tools/callcount.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Script to count calls to each subroutine
-set -e
-grep -h "call " src/*.z80 | awk '{$1=$1};1' | sort | uniq -c | sort -nr | head -n10
--- gameboy/tools/topcalls.py
+++ gameboy/tools/topcalls.py
@@ -28,6 +28,8 @@ an unconditional jp, jr, or ret) as the subroutine's caller.
 2021-03-10 (DY): Add least common calls; add jp; ignore comments;
 ignore condition (z, nz, c, nc); rewrite from awk to Python because
 I found adding these new features in Python quicker than learning awk
+
+2023-09-25 (DY): Treat fallthrough (SAVE keyword) as jp for once list
 """
 import sys, re, argparse
 from collections import defaultdict
@@ -36,7 +38,7 @@ re_debugging = False
 
 callRE = re.compile(r"""
     \s+(?:[.]?[a-zA-Z_][a-zA-Z0-9]*\s*:\s*)?  # Label
-    (jp|call)\s+  # Jump or call
+    (jp|call|fallthrough)\s+  # Jump or call
     (n?[zc]\s*,\s*)?  # Condition
     ([a-zA-Z_][a-zA-Z0-9_]*  # Label (excluding same-proc local labels)
       (?:[.][a-zA-Z_][a-zA-Z0-9_]*)?  # Label local part
@@ -53,9 +55,8 @@ def parse_argv(argv):
 def main(argv=None):
     args = parse_argv(argv or sys.argv)
 
-    # found is a dict from callee names to caller lists.
-    # {callee name: [(caller filename, line number, line), ...], ...}
-    found = defaultdict(list)
+    # {callee name: [(caller filename, line number, line text), ...], ...}
+    callees_by_name = defaultdict(list)
     
     for filename in args.filenames:
         with open(filename, "r", encoding="utf-8") as infp:
@@ -72,24 +73,25 @@ def main(argv=None):
 
         for i, (line, m) in enumerate(lines):
             if not m: continue
-            callee_name = m.group(3)
-            found[callee_name].append((filename, i + 1, line.strip()))
+            calls = callees_by_name[m.group(3)]
+            calls.append((filename, i + 1, line.strip()))
 
-    found = sorted(found.items(), key=lambda x: len(x[1]), reverse=True)
+    callees_by_name = sorted(callees_by_name.items(),
+                             key=lambda x: len(x[1]), reverse=True)
     print("Top 10 callees")
     print("\n".join(
         "%5d %s" % (len(calls), callee_name)
-        for callee_name, calls in found[:10]
+        for callee_name, calls in callees_by_name[:10]
     ))
     print("Called only once")
     print("\n".join(
         "%s:%d: %s" % calls[0]
-        for callee_name, calls in found
-        if len(calls) < 2
+        for callee_name, calls in callees_by_name
+        if (len(calls) < 2
+            and not calls[0][2].startswith("fallthrough"))
     ))
 
 if __name__=='__main__':
-##    is_IDLE = 'idlelib' in sys.modules
     want_default_file_set = len(sys.argv) < 2
     if want_default_file_set:
         import os
